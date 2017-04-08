bb602310f9b161a74a6a3ee9762e2ec305b86f44
Irina Zernova <zernovaiv@gmail.com>
2017-04-08 17:33:58 +0400

    ансвер 1

--- answers.py
+++ answers.py
@@ -2,7 +2,7 @@ def get_answer(question):
 	a = str(question)
 	answers = {'привет':'И тебе!',"?":"ничегO", "поКа":"пока!"}
 	return answers.get(a.lower())
-
+pront('smth')
 
 if __name__ == '__main__':
     print(get_answer("?"))
